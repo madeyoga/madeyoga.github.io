2c0a781302213104100b08b8ed54763e969eec5f
madeyoga <yeogaa02@gmail.com>
2025-08-12 01:06:07 +0800

    pls fix

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -2,13 +2,17 @@
 const route = useRoute()
 const normalizedPath = route.path.replace(/\/$/, '') // remove trailing slash
 const { data: post } = await useAsyncData(route.path, () => queryCollection('posts').path(normalizedPath).first())
+const { data: post2 } = await useAsyncData(normalizedPath, () => queryCollection('posts').path(route.path).first())
+
 const { data: posts } = await useAsyncData('blog_posts', () => queryCollection('posts').all())
-const { data: post2 } = await useAsyncData(route.path + "test2", () => queryCollection('posts').path(route.path).first())
+const targetPost = computed(() => posts.value?.find(p => p.path === route.path))
 
 console.log(post.value)
 console.log(posts.value)
 console.log(post2.value)
+console.log(targetPost.value)
 console.log(route.path, normalizedPath)
+
 if (!post.value) {
   throw createError({ statusCode: 404, statusMessage: 'Post not found', fatal: true })
 }
